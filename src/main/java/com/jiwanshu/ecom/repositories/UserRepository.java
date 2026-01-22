0879c68e40345af0f7cb0e3c2dd5c9c4f3131fab
jiwansh <2207340130028@recbanda.ac.in>
2026-01-22 23:50:38 +0530

    Implementing Authentication

---
+++ src/main/java/com/jiwanshu/ecom/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.jiwanshu.ecom.repositories;
+
+import com.jiwanshu.ecom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
+}
